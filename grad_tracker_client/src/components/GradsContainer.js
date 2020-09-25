1e4b1655db2f40eed8e7a1fb2c08e39299e809a6
aysan <aisayo@icloud.com>
2020-09-25 08:30:56 -0700

    created grad form

---
+++ grad_tracker_client/src/components/GradsContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import GradsForm from './GradsForm'
+
+class GradsContainer extends Component {
+    render() {
+        return (
+            <div>
+                <GradsForm />
+            </div>
+        );
+    }
+}
+
+export default GradsContainer;
---
+++ grad_tracker_client/src/components/GradsForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import { addGrad } from '../actions/gradsActions'
+
+class GradsForm extends Component {
+
+    state = { 
+        name: '',
+        degree: '',
+        grad_date: '',
+        school: ''
+    }
+
+    handleChange = e => {
+        const { name, value } = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.addGrad(state)
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handSubmit}>
+                <label>Name</label>
+                <input type='text' value={this.state.name} onChange={this.handleChange} name='name'/>
+                <br/>
+
+                <label>Degree</label>
+                <input type='text' value={this.state.degree} onChange={this.handleChange} name='degree'/>
+                <br/>
+
+                <label>Grad Date</label>
+                <input type='text' value={this.state.grad_date} onChange={this.handleChange} name='grad_date'/>
+                <br/>
+
+                <label>School</label>
+                <input type='text' value={this.state.school} onChange={this.handleChange} name='school'/>
+                <br/>
+
+                <input type='submit' value='Create Grad' />
+
+
+            </form>
+        );
+    }
+}
+
+export default connect(null, { addGrad })(GradsForm);
